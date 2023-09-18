9d695f32ebe0bf4440033161dc4f5382ac54aa91
chansoo <chansoo38@gmail.com>
2023-09-18 15:56:30 +0900

    # POST 방식에서 text를 Body에 담아 요청을 보낸경우, 해당 내용을 조회하는 Servlet을 구현한다.

---
+++ servletExcercise/src/main/java/com/example/servletexcercise/basic/request/RequestBodyStringServlet.java
@@ -0,0 +1,27 @@
+package com.example.servletexcercise.basic.request;
+
+import org.springframework.util.StreamUtils;
+
+import javax.servlet.ServletException;
+import javax.servlet.ServletInputStream;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.nio.charset.StandardCharsets;
+
+@WebServlet(name = "requestBodyStringServlet", urlPatterns = "/request-body-string")
+public class RequestBodyStringServlet extends HttpServlet {
+    @Override
+    protected void service(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        ServletInputStream inputStream = req.getInputStream();
+
+        //항상 바이트를 문자형태로 바꿀때는 인코딩정보를 알려줘여햔다 //
+        String messageBody = StreamUtils.copyToString(inputStream, StandardCharsets.UTF_8);
+
+        System.out.println("messageBody = " + messageBody);
+        resp.getWriter().write("ok");
+
+    }
+}
