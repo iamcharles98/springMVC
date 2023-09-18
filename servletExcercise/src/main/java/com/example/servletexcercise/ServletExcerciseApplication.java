33483f4f33023cf2768d6a8ba77865497474f9dd
chansoo <chansoo38@gmail.com>
2023-09-18 12:59:52 +0900

    # Initiate SpringProject

---
+++ servletExcercise/src/main/java/com/example/servletexcercise/ServletExcerciseApplication.java
@@ -0,0 +1,13 @@
+package com.example.servletexcercise;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ServletExcerciseApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ServletExcerciseApplication.class, args);
+    }
+
+}
---
+++ servletExcercise/src/test/java/com/example/servletexcercise/ServletExcerciseApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.servletexcercise;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServletExcerciseApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
