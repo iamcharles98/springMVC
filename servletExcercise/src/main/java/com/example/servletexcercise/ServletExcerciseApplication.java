53c411561d84cd2a3413cacefc6114750b321d87
chansoo <chansoo38@gmail.com>
2023-09-18 15:28:34 +0900

    # Application에 Servlet Component를 스캔하도록 설정한다

--- servletExcercise/src/main/java/com/example/servletexcercise/ServletExcerciseApplication.java
+++ servletExcercise/src/main/java/com/example/servletexcercise/ServletExcerciseApplication.java
@@ -2,8 +2,10 @@ package com.example.servletexcercise;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.ServletComponentScan;
 
 @SpringBootApplication
+@ServletComponentScan // 하위 패키지의 서블릿을 찾아서 등록해준다 //
 public class ServletExcerciseApplication {
 
     public static void main(String[] args) {
