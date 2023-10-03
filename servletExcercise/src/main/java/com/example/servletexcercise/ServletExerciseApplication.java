be391310d5645260f42c45671455c35e775db925
chansoo <chansoo38@gmail.com>
2023-10-04 00:44:22 +0900

    # Exercise 오타 수정

--- servletExcercise/src/main/java/com/example/servletexcercise/ServletExerciseApplication.java
+++ servletExcercise/src/main/java/com/example/servletexcercise/ServletExerciseApplication.java
@@ -6,10 +6,10 @@ import org.springframework.boot.web.servlet.ServletComponentScan;
 
 @SpringBootApplication
 @ServletComponentScan // 하위 패키지의 서블릿을 찾아서 등록해준다 //
-public class ServletExcerciseApplication {
+public class ServletExerciseApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(ServletExcerciseApplication.class, args);
+        SpringApplication.run(ServletExerciseApplication.class, args);
     }
 
 }
