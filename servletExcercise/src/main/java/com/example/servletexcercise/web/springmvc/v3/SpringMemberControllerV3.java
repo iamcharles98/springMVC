598d2639c8930e8301a3d322f7bc7b38e09b0b73
chansoo <chansoo38@gmail.com>
2023-10-06 00:57:02 +0900

    # 기존의 ModelAndView 객체를 반환하지 않고 viewName만 반환해 실용적으로 구현한다.   + GetMapping, PostMapping 을 이용한 컨트롤러를 구현한다.   + Model 객체를 활용해 View에 필요한 데이터를 담는다.

---
+++ servletExcercise/src/main/java/com/example/servletexcercise/web/springmvc/v3/SpringMemberControllerV3.java
@@ -0,0 +1,45 @@
+package com.example.servletexcercise.web.springmvc.v3;
+
+import com.example.servletexcercise.domain.member.Member;
+import com.example.servletexcercise.domain.member.MemberRepository;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.ModelAndView;
+
+import java.util.List;
+
+@Controller
+@RequestMapping("/springmvc/v3/members")
+public class SpringMemberControllerV3 {
+    private MemberRepository memberRepository = MemberRepository.getInstance();
+
+//    @RequestMapping(value = "/new-form", method = RequestMethod.GET)
+    @GetMapping("/new-form")
+    public String newForm() {
+        return "new-form";
+    }
+
+//    @RequestMapping(value = "/save", method = RequestMethod.POST)
+    @PostMapping("/save")
+    public String save(
+            @RequestParam("username") String username,
+            @RequestParam("age") int age,
+            Model model) {
+
+        Member member = new Member(username, age);
+        memberRepository.save(member);
+
+        model.addAttribute("member", member);
+
+        return "save-result";
+    }
+
+//    @RequestMapping(method = RequestMethod.GET)
+    @GetMapping()
+    public String members(Model model) {
+        List<Member> members = memberRepository.findAll();
+        model.addAttribute("members", members);
+        return "members";
+    }
+}
