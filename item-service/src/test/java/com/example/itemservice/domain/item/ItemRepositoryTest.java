dfdd33bdf1b353b74b77eecf7d4e046ddae9e366
chansoo <chansoo38@gmail.com>
2023-10-15 21:01:28 +0900

    # 스프링 MVC - 상품 도메인 모델 및 상품 저장소를 구현한 후 테스트를 완료한다.

---
+++ item-service/src/test/java/com/example/itemservice/domain/item/ItemRepositoryTest.java
@@ -0,0 +1,66 @@
+package com.example.itemservice.domain.item;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.*;
+
+class ItemRepositoryTest {
+    ItemRepository itemRepository = new ItemRepository();
+
+    @AfterEach
+    void afterEach() {
+        itemRepository.clearStore();
+    }
+
+    @Test
+    void save() {
+        //given
+        Item item = new Item("itemA", 10000, 10);
+        //when
+        Item savedItem = itemRepository.save(item);
+
+        //then
+        Item findItem = itemRepository.findById(item.getId());
+        assertThat(findItem).isEqualTo(savedItem);
+
+    }
+
+    @Test
+    void findALl() {
+        //given
+        Item item1 = new Item("item1", 10000, 10);
+        Item item2 = new Item("item2", 20000, 20);
+
+        itemRepository.save(item1);
+        itemRepository.save(item2);
+        //when
+        List<Item> items = itemRepository.findALl();
+
+        //then
+        assertThat(items.size()).isEqualTo(2);
+        assertThat(items).contains(item1, item2);
+    }
+
+    @Test
+    void update() {
+        Item item = new Item("item1",10000,10);
+        Item save = itemRepository.save(item);
+        Long itemId = save.getId();
+
+        Item updateParam = new Item("item2",20000,20);
+        itemRepository.update(itemId,updateParam);
+
+        Item findItem = itemRepository.findById(itemId);
+        assertThat(findItem.getItemName()).isEqualTo(updateParam.getItemName());
+        assertThat(findItem.getPrice()).isEqualTo(updateParam.getPrice());
+        assertThat(findItem.getQuantity()).isEqualTo(updateParam.getQuantity());
+
+
+    }
+
+}
